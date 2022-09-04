2ce4e325d50a53a16f5ad047dd4203b394e52de5
Weebao <baochidangg@gmail.com>
2022-09-04 10:55:02 -0400

    fix input field and html issues

--- src/components/Display.js
+++ src/components/Display.js
@@ -10,20 +10,20 @@ function Display({tipPerPerson, totalPerPerson}) {
 
   return (
     <div>
-      <span className="display-amount">
+      <div className="display-amount">
         <div className="display-label">
           <h4>Tip Amount</h4>
           <h5>/ person</h5>
         </div>
         <h3>{formatter.format(tipPerPerson)}</h3>
-      </span>
-      <span className="display-amount">
+      </div>
+      <div className="display-amount">
         <div className="display-label">
           <h4>Total</h4>
           <h5>/ person</h5>
         </div>
         <h3>{formatter.format(totalPerPerson)}</h3>
-      </span>
+      </div>
     </div>
   );
 }
